a396941882707b4fca36f0e5a6e4aa623749e5f5
Prem Kumar Yadav <premyadav5040@gmail.com>
2025-04-02 15:29:15 +0530

    Updated

--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 import com.scm.services.impl.SecurityCustomUserDetailService;
 
 @Configuration
